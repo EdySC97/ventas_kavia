076769376886f3fe5664a55d16470ecbaa0c5167
EdySC97 <131077608+EdySC97@users.noreply.github.com>
2026-08-06 15:11:25 -0600

    Update conexion.py

--- database/conexion.py
+++ database/conexion.py
@@ -9,20 +9,12 @@ os.environ["PGCLIENTENCODING"] = "utf-8"
 # 1. TUS CREDENCIALES DE POSTGRESQL
 # ==========================================
 USER = "postgres"
-PASSWORD = "S6363cf59710"  # <--- Tu contraseña correcta (recuerda que me habías puesto S6363cf5 y luego S6363cf59710)
+PASSWORD = "S6363cf59710"
 HOST = "db.jfjlasaxvictckwwzbqv.supabase.co"
 PORT = "5432"
 DB_NAME = "postgres"
-# ==========================================
-# 2. URL Y MOTOR DE SQLALCHEMY
-# ==========================================
-# Usamos quote_plus por seguridad y el driver psycopg2 que ya está instalado
-password_segura = quote_plus(PASSWORD)
-DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}"
-# Creamos el motor de SQLAlchemy
-engine = create_engine(
-    DATABASE_URL,
-    pool_pre_ping=True,  # Verifica que la conexión responda antes de usarla
-)
 
+DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
 # ==========================================
