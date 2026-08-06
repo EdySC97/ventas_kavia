1b8b53bbc05fdc02f6214ec0ec7e0f1f3a01a3dc
EdySC97 <131077608+EdySC97@users.noreply.github.com>
2026-08-06 15:13:24 -0600

    Update conexion.py

--- database/conexion.py
+++ database/conexion.py
@@ -2,19 +2,10 @@ import os
 from urllib.parse import quote_plus
 from sqlalchemy import create_engine, text
 
-# Forzamos la codificación del cliente para evitar errores de caracteres en Windows
-os.environ["PGCLIENTENCODING"] = "utf-8"
-
-# ==========================================
-# 1. TUS CREDENCIALES DE POSTGRESQL
-# ==========================================
 USER = "postgres"
 PASSWORD = "S6363cf59710"
 HOST = "db.jfjlasaxvictckwwzbqv.supabase.co"
-PORT = "5432"
+PORT = "6543"  # <--- Cambiar del puerto 5432 al 6543 (Pooler)
 DB_NAME = "postgres"
 
 DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}"
-
-engine = create_engine(DATABASE_URL)
-# ==========================================
